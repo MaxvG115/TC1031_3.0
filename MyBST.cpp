2bdff944ed610fda030e741e00246b6f9ed6e044
Maximiliano Villegas <73923032+MaxvG115@users.noreply.github.com>
2021-11-07 15:04:35 -0600

    Update Autores

--- MyBST.cpp
+++ MyBST.cpp
@@ -1,7 +1,11 @@
-//******************************
-//++Autor:Vicente Javier Viera Guízar
-//++ID=A01639784
-//******************************
+/*===============================*
+<Autores>
++Vicente Viera Guízar, ID: A01639784
++Maximiliano Villegas Garcia. ID: A01635825
++Emma Gabriela Alfaro De la Rocha. ID: A01740229
+</Autores>
+=================================*
+*/
 
 #include "MyBST.h"
 #include <queue>
