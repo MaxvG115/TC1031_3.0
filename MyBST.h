b2f6514a35266ce653fdb18115e73fe06ea4788d
Maximiliano Villegas <73923032+MaxvG115@users.noreply.github.com>
2021-11-07 17:10:46 -0600

    Mejora de limpieza

--- MyBST.h
+++ MyBST.h
@@ -14,47 +14,46 @@
 #include <string>
 
 struct MyNodeBST{
-    int data;
-    MyNodeBST* left;
-    MyNodeBST* right;
-    std::vector<std::string> ipes;
+  int data;
+  MyNodeBST* left;
+  MyNodeBST* right;
+  std::vector<std::string> ipes;
 
-    MyNodeBST(int data){
-        this->data=data;
-        this->left=this->right=nullptr;
-    }
+  MyNodeBST(int data){
+    this->data=data;
+    this->left=this->right=nullptr;
+  }
 };
 
 class MyBST{
-    private:
-        MyNodeBST* root;
-        int size;
-        void preorder(MyNodeBST*);
-        void postorder(MyNodeBST*);
-        void inorder(MyNodeBST*);
-        void lvl(MyNodeBST*);
-        int height(MyNodeBST*);
-        int whatLevelIAm(int,MyNodeBST*);
-        int steps(int,MyNodeBST*);
-        MyNodeBST* insert(MyNodeBST*,int,std::string);
-        MyNodeBST* remove(MyNodeBST*,int);
-        bool ancestors(MyNodeBST*,int);
-        bool search(int,MyNodeBST*);
-        int reverseOrder(MyNodeBST*,int);
-    public:
-        int level;
-        MyBST();
-        int getSize();
-        bool isEmpty();
-        bool search(int);
-        void visit(int);
-        int height();
-        int whatLevelIAm(int);
-        bool insert(int,std::string);
-        void ancestors(int);
-        MyNodeBST* succesor(MyNodeBST*);
-        bool remove(int);
-        int reverseOrder(int);
+  private:
+    MyNodeBST* root;
+    int size;
+    void preorder(MyNodeBST*);
+    void postorder(MyNodeBST*);
+    void inorder(MyNodeBST*);
+    void lvl(MyNodeBST*);
+    int height(MyNodeBST*);
+    int whatLevelIAm(int,MyNodeBST*);
+    int steps(int,MyNodeBST*);
+    MyNodeBST* insert(MyNodeBST*,int,std::string);
+    MyNodeBST* remove(MyNodeBST*,int);
+    bool ancestors(MyNodeBST*,int);
+    bool search(int,MyNodeBST*);
+    int reverseOrder(MyNodeBST*,int);
+  public:
+    int level;
+    MyBST();
+    int getSize();
+    bool isEmpty();
+    bool search(int);
+    void visit(int);
+    int height();
+    int whatLevelIAm(int);
+    bool insert(int,std::string);
+    void ancestors(int);
+    MyNodeBST* succesor(MyNodeBST*);
+    bool remove(int);
+    int reverseOrder(int);
 };
-
 #endif
