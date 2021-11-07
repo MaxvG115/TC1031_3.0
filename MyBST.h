7d210a50b6d8f6060b69decdb2a8f390e7217f56
Maximiliano Villegas <73923032+MaxvG115@users.noreply.github.com>
2021-11-07 15:05:06 -0600

    Update Autores

--- MyBST.h
+++ MyBST.h
@@ -1,3 +1,11 @@
+/*===============================*
+<Autores>
++Vicente Viera Guízar, ID: A01639784
++Maximiliano Villegas Garcia. ID: A01635825
++Emma Gabriela Alfaro De la Rocha. ID: A01740229
+</Autores>
+=================================*
+*/
 #ifndef MYBST
 #define MYBST
 
